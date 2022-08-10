a50cd45afe5d87cf4311bc7ef54cfcaacfd32efa
Suffyan101 <suffyan.r01@gmail.com>
2022-08-11 03:02:02 +0500

    update

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-var calinput= document.getElementById("cal-input");
+var calinput = document.getElementById("cal-input");
 
-function on_click(val){
-    // if(calinput.value === "")
-    calinput.value += val
+function on_click(val) {
+    if (val === '+' || val === '-' || val === '/' || val === '*') {
+        var leth = document.getElementById("cal-input").value.length;
+        var len = leth-1;
+        console.log(len)
+        if (len != "+" || len != "-" || len != "/" || len != "*") {
+            calinput.value += val
+            
+        }
+        else {
+            // calinput.value -= val
+            calinput.value[len] += val
+        }
+    }
+    else{
+        calinput.value += val
+    }
 }
-function clearValue(){
+
+function clearValue() {
     calinput.value = ""
 }
-function calcul(){
+function calcul() {
     calinput.value = eval(calinput.value)
 }
